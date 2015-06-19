f4eb5caf5d0df8f8afdc2768612ad753b6dcdb09
Dadsaster <olliebradley76.com>
2015-06-19 16:36:20 -0700

    Adding new completed exercises from LPTHW

---
+++ ex18.py
@@ -0,0 +1,23 @@
+__author__ = 'Oliver'
+
+# this one is like your scripts with argv
+def print_two(*args):
+    arg1, arg2 = args
+    print ("arg1: {}, arg2: {}").format(arg1, arg2)
+
+# ok, that *args is actually pointless, we can just do this
+def print_two_again(arg1, arg2):
+    print ("arg1: {}, arg2: {}").format(arg1, arg2)
+
+# this just takes one argument
+def print_one(argv1):
+    print("arg1: {}").format(argv1)
+
+#this one takes no arguments
+def print_none():
+    print("I got nothin'.")
+
+print_two("Zed", "Shaw")
+print_two_again("Zed", "Shaw")
+print_one("First!")
+print_none()
